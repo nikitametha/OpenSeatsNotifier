6dca48887186b7a2981c245f3886a57a49ff29a9
Nikita Metha <nikitametha@gmail.com>
2023-08-24 09:06:35 -0700

    Updated README and cleaned up existing script

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 A Course Seat Availability Monitor
 
+Originally created for notifying the author about available seats for the 5xx courses offered at ASU.
+
 ## Authors
 
 - [@nikitametha](https://nikitametha.com) :)
@@ -10,9 +12,18 @@ A Course Seat Availability Monitor
 
 
 
-## Installation
+## Features
 
-1) Install requirements
+- Checks for non reserved available seats at the ASU class search website.
+- Originally created for CSE 5xx courses but can be used for any stream!
+- Notifies user via email if any courses are available.
+
+
+
+
+## Installation
+1) Clone the project 
+2) Install requirements
 
 ```bash
   pip install -r requirements.txt
@@ -21,12 +32,27 @@ A Course Seat Availability Monitor
 2) Install latest Chromedriver here:
 https://chromedriver.chromium.org/downloads
     
-## Deployment
+## Usage
+In the folder ```OpenSeatsNotifier```
+- Make sure you have chrome driver installed in the same folder.
+- Change the link at ```driver.get("<link>")``` to the class search link of your stream. (The default is grad CSE courses.)
+- <TODO - Add your email at ..>
 
-To run this project, simply run the script:
-
+To run the script
 ```bash
-  cd ~/<location/to/folder>/../OpenSeatsNotifier
   python open_seats.py
 ```
+
+## Roadmap
+
+- Add ability for user to:
+    - Choose the exact courses that they want to be notified on.
+    - Specify the stream/discipline and level of course (grad, undergrad..)
+- Containerize application to deploy script onto multiple systems with ease
+- Configure cron jobs in containers for script to run every few minutes - make it as real time as possible.
+
+
+## Tech Stack
+
+Python, BeautifulSoup4, Selenium, ChromeDriver
 
--- open_seats.py
+++ open_seats.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup as bs
-import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -12,52 +11,53 @@ from selenium.webdriver.common.by import By
 
 import time
 
+
 print("ASU SEATS")
 driver = webdriver.Chrome()
 driver.get("https://catalog.apps.asu.edu/catalog/classes/classlist?advanced=true&campusOrOnlineSelection=C&honors=F&level=grad&promod=F&searchType=open&subject=CSE&term=2237")
-time.sleep(2)
+time.sleep(2) #waiting for page to open
 source = driver.page_source
 soup = bs(source, "lxml")
+
 
 course_names = []
 cn1 = []
 cn2 = []
 available_open_seats=[]
 
-
-
-
 for td in soup.findAll("td", { "class":"titleColumnValue" }):
+        #html parsing magic - you'll understand it if you look at the HTML source code on the website :)
         cn1 = (str(td)).split('i>')
         cn2 = (str(cn1[2])).split('<span')
         ress = cn2[0].replace(' ', '').replace('\n', '').replace('\t', '').replace('<!--', '')
+        # for some reason CSE 598 shows up empty
         if ress=='':
             ress="Course 598"
         course_names.append(ress)
+
 
 ras_button = driver.find_elements(by=By.XPATH, value="//*[contains(@class, 'bold-hyperlink')]")
 
 for x in range(len(ras_button)):
         
         try:
-            
-            txt=ras_button[x].text
-            if(txt.startswith("CSE 5") and (not txt.startswith("CSE 59")) and (not txt.startswith("CSE 58"))):
-                    print(txt)
-                    print(ras_button[x+1].text)
+            course_number=ras_button[x].text
+            if(course_number.startswith("CSE 5") and (not course_number.startswith("CSE 59")) and (not course_number.startswith("CSE 58"))):
+                    print(course_number) #print course number
+                    print(ras_button[x+1].text) #print course name
                     ras_button[x].click()
-                    #print("clicked?")
+                    #wait for details to load
                     time.sleep(2)
                     x1 = driver.find_elements(by=By.XPATH, value="//*[contains(@class, 'table table-sm reserved-seats')]")
-                    #print("stuff=",x1[-1].text[-15:])
-                    rass = ((x1[-1].text).split(' ')[-1])
-                    available_open_seats.append(rass)
-                    print ("Seats = ", rass)
-                    intrass = int(rass)
-                    #if(intrass>0 and intrass<20):
+                    #parsing the element x1
+                    #non reserved available seats
+                    available_seats_nr = ((x1[-1].text).split(' ')[-1])
+                    available_open_seats.append(int(available_seats_nr))
+                    print ("Seats = ", available_seats_nr)
+                    #if(available_seats_nr>0 and available_seats_nr<20):
                         #send email notification : ToDo
         except:
-            available_open_seats.append("0")
+            available_open_seats.append(0)
 
 
 
